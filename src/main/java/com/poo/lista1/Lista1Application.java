168295c8be7daebd598f03a1036155d3f015d914
elipekkkj <luiscaetano18180909@gmail.com>
2024-08-30 21:25:59 -0300

    first commit

---
+++ src/main/java/com/poo/lista1/Lista1Application.java
@@ -0,0 +1,15 @@
+package com.poo.lista1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista1Application.class, args);
+	
+	System.out.println("Hello Word!");
+
+	}
+}
