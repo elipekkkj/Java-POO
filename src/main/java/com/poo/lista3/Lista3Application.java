fb730d5dfe368f899e5b0688483826d9aac440f1
elipekkkj <luiscaetano18180909@gmail.com>
2024-09-03 17:48:47 -0300

    Exercicio 1

---
+++ src/main/java/com/poo/lista3/Lista3Application.java
@@ -0,0 +1,15 @@
+package com.poo.lista3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista3Application {
+
+	public static void main(String[] args) throws InterruptedException { 
+		SpringApplication.run(Lista3Application.class, args); 
+
+	//Exercicio1.resolucao();
+	
+	}
+}
