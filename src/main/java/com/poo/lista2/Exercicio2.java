c77d5d691dc4aaf0e7714159b634a73af020caf7
elipekkkj <luiscaetano18180909@gmail.com>
2024-09-03 14:48:00 -0300

    Exercicio 2

--- src/main/java/com/poo/lista2/Exercicio2.java
+++ src/main/java/com/poo/lista2/Exercicio2.java
@@ -1,26 +1,28 @@
 package com.poo.lista2;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
-    public static void resolucao(){
+   public static void resolucao(){
 
-        int num1, num2;
-        
-        System.out.println("Digite um numero: ");
-        num1=leia.nextInt();
-        System.out.println("Digite outro numero: ");
-        num2=leia.nextInt();
+   int num1, num2;
+   
+   System.out.println("Digite um numero: ");
+   Scanner leia = new Scanner(System.in);
+   num1=leia.nextInt();
+   System.out.println("Digite outro numero: ");
+   num2=leia.nextInt();
 
-        if(num1 > num2){
-        System.out.println("Número 1 maior que número 2");
-        }
-        else if(num1 < num2){
-           System.out.println("Número 1 menor que número 2");
-        }
-        else{
-           System.out.println("Número 1 igual a número 2");
-        }
-       
-       leia.close(); 
-
-    }
+   if(num1 > num2){
+   System.out.println("Número 1 maior que número 2");
+   }
+   else if(num1 < num2){
+   System.out.println("Número 1 menor que número 2");
+   }
+   else{
+   System.out.println("Número 1 igual a número 2");
+   }
+   leia.close(); 
+   
+   }
 }
