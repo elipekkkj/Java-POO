4accb5e2ba8967b4c45bf42ae7a6f008b56f588c
elipekkkj <luiscaetano18180909@gmail.com>
2024-09-04 15:41:20 -0300

    Exercicio 3

--- src/main/java/com/poo/lista3/Exercicio3.java
+++ src/main/java/com/poo/lista3/Exercicio3.java
@@ -5,23 +5,27 @@ import java.util.Scanner;
 public class Exercicio3 {
     public static void resolucao(){
 
-        int num1, num2;
-        int pares;
+    
+    Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro número: ");
-        Scanner leia = new Scanner(System.in);
-        num1 = leia.nextInt();
-        
-        System.out.println("Digite o segundo número: ");
-        num2 = leia.nextInt();
-        
-        System.out.println("Numeros pares entre " + num1 + " e " + num2 + ": ");
+    System.out.println("Digite o primeiro número:");
+    int num1 = scanner.nextInt();
+    System.out.println("Digite o segundo número:");
+    int num2 = scanner.nextInt();
 
-        for (int i = num1; i <= num2; i++) {
+    int inicio = Math.min(num1, num2);
+    int fim = Math.max(num1, num2);
 
-            if(i % 2==0){
-                System.out.println(i);
-            }
+    System.out.println("Números pares entre " + inicio + " e " + fim + ":");
+
+    for (int i = inicio; i <= fim; i++) {
+        if (i % 2 == 0) {
+            System.out.println(i);
         }
     }
+
+    scanner.close();
+    }
 }
+
+
