e2010f13759dec5ec6b2296c56a5c17bf6f01094
elipekkkj <luiscaetano18180909@gmail.com>
2024-09-05 17:41:24 -0300

    Exercicio 4

--- src/main/java/com/poo/lista4/Exercicio4.java
+++ src/main/java/com/poo/lista4/Exercicio4.java
@@ -3,11 +3,14 @@ package com.poo.lista4;
 public class Exercicio4 {
     public static void resolucao(){
         int lista[] = {3,5,6,7,8,10,22,55,110,133,166,185,210,235,320,324,444};
+        int contaPares = 0;
 
         for (int i = 0; i <= 16 ; i++) {
             if (lista[i] % 2 == 0) {
-                System.out.println(lista[i]);
+                lista[i] += contaPares;
+                contaPares++;
             }
         }
+        System.out.println("Quantidade de números pares presentes no vetor: " + contaPares);
     }
 }
