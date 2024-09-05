a6c4d2c671194157fa407a65ff47051f914eae13
elipekkkj <luiscaetano18180909@gmail.com>
2024-09-05 15:32:06 -0300

    Exercicio 4

---
+++ src/main/java/com/poo/lista4/Exercicio1.java
@@ -0,0 +1,27 @@
+package com.poo.lista4;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void resolucao(){
+
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Número de vendas do dia: ");
+        int numeroVendas = leia.nextInt();
+
+        double vendas[] = new double [numeroVendas];
+        double totalVendas = 0;
+
+        for (int i = 0; i <= numeroVendas; i++) {
+            System.out.println("Digite o valor da venda " + (i + 1) + ":");
+            vendas[i] = leia.nextDouble();
+        }
+        for (double venda : vendas) {
+            totalVendas += venda;
+        }
+        System.out.println("Total das vendas do dia: R$" + totalVendas);
+
+    leia.close();
+    }
+}
---
+++ src/main/java/com/poo/lista4/Lista4Application.java
@@ -0,0 +1,14 @@
+package com.poo.lista4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista4Application {
+
+	public static void main(String[] args) { 
+		SpringApplication.run(Lista4Application.class, args);
+		
+		//Exercicio1.resolucao();
+	}
+}
